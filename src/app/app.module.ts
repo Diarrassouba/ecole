aaa3409f0ce32a11d90d118600ce6ebffef537f5
Diarrassouba Drissa <ddrissaci@gmail.com>
2016-12-15 15:05:08 +0000

    Création des composants personne, etudiant, enseignan et administrateur.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PersonneComponent } from './personne/personne.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { AdministrateurComponent } from './administrateur/administrateur.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonneComponent,
+    EtudiantComponent,
+    AdministrateurComponent,
+    EnseignantComponent
   ],
   imports: [
     BrowserModule,
