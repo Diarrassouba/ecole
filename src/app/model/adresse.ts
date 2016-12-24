f1d9c1ed002af42319000a3a5a9233de4696ee9d
Diarrassouba Drissa <ddrissaci@gmail.com>
2016-12-24 19:09:19 +0000

    Creations route

--- src/app/model/adresse.ts
+++ src/app/model/adresse.ts
@@ -1,2 +1,12 @@
 export class Adresse {
+  private _quartier:string;
+  private _contact:string[];
+  private _email:string;
+
+
+  constructor(quartier: string='', contact: string[]=[], email: string='') {
+    this._quartier = quartier;
+    this._contact = contact;
+    this._email = email;
+  }
 }
