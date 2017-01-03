cc0de7d38e881f41e65968f6bfb8322a4fab1fc6
Diarrassouba Drissa <ddrissaci@gmail.com>
2017-01-03 21:29:42 +0000

    Creation accueil suite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PersonneComponent} from "./personne/personne.component";
+import {AccueilComponent} from "./accueil/accueil.component";
 
 const routes: Routes = [
+  {path:"", component: AccueilComponent},
+  {path:"accueil", component: AccueilComponent},
   {path: "invites", component: PersonneComponent}
 ];
 
@@ -13,6 +16,5 @@ const routes: Routes = [
 })
 export class AppRoutingModule {
 
-  constructor() {
-  }
+  constructor() {}
 }
